aebf3695e624e447113a23b44d6e6a921f20dd14
SungyeopJeong <syeopj4a@gmail.com>
2024-01-05 16:59:11 +0000

    kakao 로그인 연결

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const axios = require('axios')
+const axios = require("axios");
 
 router.post("/google", async (req, res) => {
     var token = req.body.token;
-    const url = "https://www.googleapis.com/oauth2/v2/userinfo"
-    const resp = await axios.get(url, {
-        headers: {
-            'Authorization': `Bearer ${token}`,
-        }
-    })
-    res.json(resp.data)
+    console.log(token);
+    const url = "https://www.googleapis.com/oauth2/v2/userinfo";
+    try {
+        const resp = await axios.get(url, {
+            headers: { "Authorization": `Bearer ${token}` }
+        });
+        res.json(resp.data);
+    } catch (error) {
+        res.sendStatus(401);
+    }
 });
 
-module.exports = router
+router.post("/kakao", (req, res) => {
+    var token = req.body.token;
+    console.log(token);
+    res.sendStatus(200);
+});
+
+module.exports = router;
