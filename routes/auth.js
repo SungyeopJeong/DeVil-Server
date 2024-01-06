3aef834740acff545f614670da41a7bdd31e2871
SungyeopJeong <syeopj4a@gmail.com>
2024-01-06 17:13:24 +0900

    구글 user 정보 가져오기

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const request = require("request");
+const db = require("../db/db")
 require("dotenv").config()
 
-function login(id) {
-
+function login(id, username, platform) {
+  console.log(id, username, platform);
 }
 
 router.post("/google", (req, res) => {
   request.get({
-    url: process.env.AUTH_GOOGLE_URL,
+    url: process.env.AUTH_GOOGLE_TOKEN_URL,
     qs: { access_token: req.body.token },
   }, (_error, _response, body) => {
-    const id = JSON.parse(body).sub;
-    if (!id || JSON.parse(body).aud != process.env.AUTH_GOOGLE_CLIENT_ID) res.sendStatus(401);
+    const parsedBody = JSON.parse(body);
+    const id = parsedBody.sub;
+    if (!id || parsedBody.aud != process.env.AUTH_GOOGLE_CLIENT_ID) res.sendStatus(401);
     else {
-      login(id);
-      res.sendStatus(200);
+      request.get({
+        url: process.env.AUTH_GOOGLE_USER_URL,
+        qs: { access_token: req.body.token },
+      }, (_error, _response, body) => {
+        const name = JSON.parse(body).name;
+        if (!name) res.sendStatus(401);
+        login(id, name, "google");
+        res.sendStatus(200);
+      })
     }
   });
 });
@@ -28,10 +37,11 @@ router.post("/kakao", async (req, res) => {
       Authorization: `Bearer ${req.body.token}`,
     },
   }, (_error, _response, body) => {
-    const id = JSON.parse(body).id;
+    const parsedBody = JSON.parse(body);
+    const id = parsedBody.id;
     if (!id) res.sendStatus(401);
     else {
-      login(id.toString());
+      login(id.toString(), parsedBody.properties.nickname, "kakao");
       res.sendStatus(200);
     }
   });
