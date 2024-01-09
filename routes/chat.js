26593ae94b85735f6b9d9eaefe52c911c52554b5
Seunghyo <shyo0218@naver.com>
2024-01-10 01:20:17 +0900

    socket setting

--- routes/chat.js
+++ routes/chat.js
@@ -4,54 +4,15 @@ const db = require("../db/db");
 const socketIO = require("socket.io");
 const http = require("http");
 
-const server = http.createServer(express);
+const app = express();
+const server = http.createServer(app);
 const io = socketIO(server);
 
-io.on("connection", (socket) => {
-  console.log("User connected");
+router.post("/", (req, res) => {
+  const id = req.body.id;
 
-  socket.on("chatUpdate", () => {
-    io.emit("chatUpdate");
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
-});
-
-// router.post("/", (req, res) => {
-//   const id = req.body.id;
-
-//   if (!id) res.sendStatus(400);
-//   else {
-//     const sql =
-//       "select username, studyid, content, timestamp, " +
-//       "case when userid = ? then true else false end as ismine " +
-//       "from studychat " +
-//       "join users on users.id = userid " +
-//       "where studyid in (SELECT studyid FROM userstudy WHERE userid = ?) " +
-//       "order by studyid, timestamp, studychat.id";
-
-//     db.query(sql, [id, id], (err, result) => {
-//       console.log(err);
-//       if (err) res.sendStatus(500);
-//       else {
-//         io.emit("chat update", result);
-//         res.status(200).send(result);
-//       }
-//     });
-//   }
-// });
-
-router.post("/", async (req, res) => {
-  try {
-    const id = req.body.id;
-
-    if (!id) {
-      res.sendStatus(400);
-      return;
-    }
-
+  if (!id) res.sendStatus(400);
+  else {
     const sql =
       "select username, studyid, content, timestamp, " +
       "case when userid = ? then true else false end as ismine " +
@@ -60,22 +21,16 @@ router.post("/", async (req, res) => {
       "where studyid in (SELECT studyid FROM userstudy WHERE userid = ?) " +
       "order by studyid, timestamp, studychat.id";
 
-    const result = await new Promise((resolve, reject) => {
-      db.query(sql, [id, id], (err, result) => {
-        if (err) {
-          console.error(err);
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      });
+    db.query(sql, [id, id], (err, result) => {
+      if (err) {
+        res.sendStatus(500);
+        console.log(err);
+      } else {
+        io.emit("chat update", result);
+        res.status(200).send(result);
+      }
     });
-
-    res.status(200).send(result);
-  } catch (error) {
-    console.error(error);
-    res.sendStatus(500);
   }
 });
 
-module.exports = { router };
+module.exports = { router, io };
