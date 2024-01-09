108ed15a5c8275abf363509eed904eb17ae19674
SungyeopJeong <syeopj4a@gmail.com>
2024-01-09 22:04:44 +0900

    Add now field to user study list

--- routes/user.js
+++ routes/user.js
@@ -7,9 +7,10 @@ router.post("/studies", (req, res) => {
 
     if (!id) res.sendStatus(400);
     else {
-        const sql = "SELECT id, name, category, description, max, " +
-            "case when creatorId = ? then true else false end as canEdit " +
-            "FROM studies WHERE id in (SELECT studyid FROM userstudy WHERE userid = ?)";
+        const sql = "SELECT id, name, category, description, count(userid) as now, max, " +
+            "case when creatorid = ? then true else false end as canDelete " +
+            "FROM studies left outer join userstudy on studyid = id " +
+            "WHERE id in (SELECT studyid FROM userstudy WHERE userid = ?) group by id";
 
         db.query(sql, [id, id], (err, result) => {
             if (err) res.sendStatus(500);
