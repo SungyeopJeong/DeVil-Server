d1ed51364ca920130eb4b66c67e322e0b5f24215
Seunghyo <shyo0218@naver.com>
2024-01-10 19:28:34 +0900

    userstudy 정렬

--- routes/user.js
+++ routes/user.js
@@ -3,20 +3,21 @@ const router = express.Router();
 const db = require("../db/db");
 
 router.post("/studies", (req, res) => {
-    const id = req.body.id;
+  const id = req.body.id;
 
-    if (!id) res.sendStatus(400);
-    else {
-        const sql = "SELECT id, name, category, description, count(userid) AS now, max, " +
-            "CASE WHEN creatorid = ? THEN TRUE ELSE FALSE END AS canDelete " +
-            "FROM studies LEFT OUTER JOIN userstudy ON studyid = id " +
-            "WHERE id IN (SELECT studyid FROM userstudy WHERE userid = ?) GROUP BY id ORDER BY id DESC";
+  if (!id) res.sendStatus(400);
+  else {
+    const sql =
+      "SELECT id, name, category, description, count(userid) AS now, max, " +
+      "CASE WHEN creatorid = ? THEN TRUE ELSE FALSE END AS canDelete " +
+      "FROM studies LEFT OUTER JOIN userstudy ON studyid = id " +
+      "WHERE id IN (SELECT studyid FROM userstudy WHERE userid = ?) GROUP BY id ORDER BY MAX(createdtime) DESC";
 
-        db.query(sql, [id, id], (err, result) => {
-            if (err) res.sendStatus(500);
-            else res.status(200).send(result);
-        });
-    }
+    db.query(sql, [id, id], (err, result) => {
+      if (err) res.sendStatus(500);
+      else res.status(200).send(result);
+    });
+  }
 });
 
 module.exports = router;
